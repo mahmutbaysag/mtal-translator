affba31e7850f878160884f1b9d4b4026041f3cf
Mahmut Baysağ <mbaysag1080@gmail.com>
2021-03-26 17:18:34 +0300

    Yedekleme

---
+++ tr2/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace tr2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroRadioButton2 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton1 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
+            this.metroRadioButton6 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton5 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton3 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton4 = new MetroFramework.Controls.MetroRadioButton();
+            this.metroPanel1.SuspendLayout();
+            this.metroPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Highlight = true;
+            this.metroButton1.Location = new System.Drawing.Point(322, 51);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(87, 23);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = "ÇEVİR";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.MetroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(114, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(201, 201);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(-1, 51);
+            this.metroTextBox1.Margin = new System.Windows.Forms.Padding(35, 30, 35, 30);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Multiline = true;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(316, 203);
+            this.metroTextBox1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.TabIndex = 1;
+            this.metroTextBox1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            this.metroTextBox1.TextChanged += new System.EventHandler(this.MetroTextBox1_TextChanged);
+            this.metroTextBox1.Click += new System.EventHandler(this.MetroTextBox1_Click);
+            this.metroTextBox1.Enter += new System.EventHandler(this.MetroButton1_Click);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(114, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(201, 201);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(416, 51);
+            this.metroTextBox2.Margin = new System.Windows.Forms.Padding(35, 30, 35, 30);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Multiline = true;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ReadOnly = true;
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(316, 203);
+            this.metroTextBox2.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroTextBox2.TabIndex = 2;
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(195, 500);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(23, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(23, 20);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("https://ceviri.yandex.com.tr/?lang=en-tr", System.UriKind.Absolute);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Highlight = true;
+            this.metroButton2.Location = new System.Drawing.Point(322, 138);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(87, 23);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroButton2.TabIndex = 6;
+            this.metroButton2.Text = "Kopyala";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.MetroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Highlight = true;
+            this.metroButton3.Location = new System.Drawing.Point(322, 109);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(87, 23);
+            this.metroButton3.TabIndex = 7;
+            this.metroButton3.Text = "Yapıştır";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.MetroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Highlight = true;
+            this.metroButton4.Location = new System.Drawing.Point(322, 80);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(87, 23);
+            this.metroButton4.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroButton4.TabIndex = 8;
+            this.metroButton4.Text = "TEMİZLE";
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.MetroButton4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.metroRadioButton2);
+            this.metroPanel1.Controls.Add(this.metroRadioButton1);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(322, 205);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(87, 39);
+            this.metroPanel1.TabIndex = 9;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroRadioButton2
+            // 
+            this.metroRadioButton2.AutoSize = true;
+            this.metroRadioButton2.Location = new System.Drawing.Point(21, 21);
+            this.metroRadioButton2.Name = "metroRadioButton2";
+            this.metroRadioButton2.Size = new System.Drawing.Size(47, 15);
+            this.metroRadioButton2.TabIndex = 3;
+            this.metroRadioButton2.Text = "Dark";
+            this.metroRadioButton2.UseSelectable = true;
+            this.metroRadioButton2.CheckedChanged += new System.EventHandler(this.MetroRadioButton2_CheckedChanged);
+            // 
+            // metroRadioButton1
+            // 
+            this.metroRadioButton1.AutoSize = true;
+            this.metroRadioButton1.Checked = true;
+            this.metroRadioButton1.Location = new System.Drawing.Point(21, 3);
+            this.metroRadioButton1.Name = "metroRadioButton1";
+            this.metroRadioButton1.Size = new System.Drawing.Size(50, 15);
+            this.metroRadioButton1.TabIndex = 2;
+            this.metroRadioButton1.TabStop = true;
+            this.metroRadioButton1.Text = "Light";
+            this.metroRadioButton1.UseSelectable = true;
+            this.metroRadioButton1.CheckedChanged += new System.EventHandler(this.MetroRadioButton1_CheckedChanged);
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.Controls.Add(this.metroRadioButton6);
+            this.metroPanel2.Controls.Add(this.metroRadioButton5);
+            this.metroPanel2.Controls.Add(this.metroRadioButton3);
+            this.metroPanel2.Controls.Add(this.metroRadioButton4);
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(322, 167);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(87, 35);
+            this.metroPanel2.TabIndex = 10;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
+            // 
+            // metroRadioButton6
+            // 
+            this.metroRadioButton6.AutoSize = true;
+            this.metroRadioButton6.Location = new System.Drawing.Point(7, 17);
+            this.metroRadioButton6.Name = "metroRadioButton6";
+            this.metroRadioButton6.Size = new System.Drawing.Size(64, 15);
+            this.metroRadioButton6.TabIndex = 5;
+            this.metroRadioButton6.Text = "TR->EN";
+            this.metroRadioButton6.UseSelectable = true;
+            // 
+            // metroRadioButton5
+            // 
+            this.metroRadioButton5.AutoSize = true;
+            this.metroRadioButton5.Checked = true;
+            this.metroRadioButton5.Location = new System.Drawing.Point(7, 3);
+            this.metroRadioButton5.Name = "metroRadioButton5";
+            this.metroRadioButton5.Size = new System.Drawing.Size(64, 15);
+            this.metroRadioButton5.TabIndex = 4;
+            this.metroRadioButton5.TabStop = true;
+            this.metroRadioButton5.Text = "EN->TR";
+            this.metroRadioButton5.UseSelectable = true;
+            // 
+            // metroRadioButton3
+            // 
+            this.metroRadioButton3.AutoSize = true;
+            this.metroRadioButton3.Location = new System.Drawing.Point(21, 21);
+            this.metroRadioButton3.Name = "metroRadioButton3";
+            this.metroRadioButton3.Size = new System.Drawing.Size(47, 15);
+            this.metroRadioButton3.TabIndex = 3;
+            this.metroRadioButton3.Text = "Dark";
+            this.metroRadioButton3.UseSelectable = true;
+            // 
+            // metroRadioButton4
+            // 
+            this.metroRadioButton4.AutoSize = true;
+            this.metroRadioButton4.Location = new System.Drawing.Point(21, 3);
+            this.metroRadioButton4.Name = "metroRadioButton4";
+            this.metroRadioButton4.Size = new System.Drawing.Size(50, 15);
+            this.metroRadioButton4.TabIndex = 2;
+            this.metroRadioButton4.Text = "Light";
+            this.metroRadioButton4.UseSelectable = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 251);
+            this.Controls.Add(this.metroPanel2);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton1);
+            this.DoubleBuffered = false;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(23, 60, 23, 20);
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Text = "M.T.A.L Translator";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Enter += new System.EventHandler(this.MetroButton1_Click);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.metroPanel2.ResumeLayout(false);
+            this.metroPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private System.Windows.Forms.Timer timer1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton2;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton1;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton6;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton5;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton3;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton4;
+    }
+}
+
---
+++ tr2/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework;
+using System.IO;
+
+
+namespace tr2
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+
+            metroRadioButton3.Enabled = false;
+            metroRadioButton4.Enabled = false;
+            metroRadioButton4.Visible = false;
+            metroRadioButton3.Visible = false;
+
+            if(metroRadioButton5.Checked==true)
+            {
+                webBrowser1.Navigate("https://ceviri.yandex.com.tr/?lang=en-tr");
+                
+            }
+            else if (metroRadioButton6.Checked==true)
+            {
+                webBrowser1.Navigate("https://ceviri.yandex.com.tr/?lang=tr-en");
+                
+            }
+
+            if (metroRadioButton1.Checked == true)
+            {
+                light();
+            }
+            if (metroRadioButton2.Checked == true)
+            {
+                dark();
+            }
+
+        }
+
+        public void light()
+        {
+            this.Theme = MetroThemeStyle.Light;
+            metroButton1.Theme= MetroThemeStyle.Light;
+            metroButton2.Theme= MetroThemeStyle.Light;
+            metroButton3.Theme= MetroThemeStyle.Light;
+            metroButton4.Theme= MetroThemeStyle.Light;
+            metroTextBox1.Theme= MetroThemeStyle.Light;
+            metroTextBox2.Theme= MetroThemeStyle.Light;
+            metroPanel1.Theme= MetroThemeStyle.Light;
+            metroRadioButton1.Theme= MetroThemeStyle.Light;
+            metroRadioButton2.Theme= MetroThemeStyle.Light;
+        }
+
+        public void dark()
+        {
+            this.Theme = MetroThemeStyle.Dark;
+            metroButton1.Theme = MetroThemeStyle.Dark;
+            metroButton2.Theme = MetroThemeStyle.Dark;
+            metroButton3.Theme = MetroThemeStyle.Dark;
+            metroButton4.Theme = MetroThemeStyle.Dark;
+            metroTextBox1.Theme = MetroThemeStyle.Dark;
+            metroTextBox2.Theme = MetroThemeStyle.Dark;
+            metroPanel1.Theme = MetroThemeStyle.Dark;
+            metroRadioButton1.Theme = MetroThemeStyle.Dark;
+            metroRadioButton2.Theme = MetroThemeStyle.Dark;
+        }
+     
+        private void MetroTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MetroButton1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            metroTextBox1.Focus();
+        }
+
+        private void MetroTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void MetroButton3_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text = Clipboard.GetText();
+        }
+
+        private void MetroButton2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(metroTextBox2.Text);
+        }
+
+        private void MetroButton4_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text = "";
+            metroTextBox2.Text = "";
+            if (metroRadioButton5.Checked == true)
+            {
+                webBrowser1.Navigate("https://ceviri.yandex.com.tr/?lang=en-tr");
+
+            }
+            else if (metroRadioButton6.Checked == true)
+            {
+                webBrowser1.Navigate("https://ceviri.yandex.com.tr/?lang=tr-en");
+
+            }
+        }
+        int sayac = 0;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            webBrowser1.Document.GetElementById("textarea").InnerText = metroTextBox1.Text;
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            if (sayac == 2)
+            {
+                metroTextBox2.Text = webBrowser1.Document.GetElementById("translation").InnerText;
+                this.webBrowser1.ScriptErrorsSuppressed = true;
+                sayac = 0;
+                timer1.Stop();
+            }
+            
+        }
+
+        private void MetroRadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Theme = MetroThemeStyle.Light;
+            metroButton1.Theme = MetroThemeStyle.Light;
+            metroButton2.Theme = MetroThemeStyle.Light;
+            metroButton3.Theme = MetroThemeStyle.Light;
+            metroButton4.Theme = MetroThemeStyle.Light;
+            metroTextBox1.Theme = MetroThemeStyle.Light;
+            metroTextBox2.Theme = MetroThemeStyle.Light;
+            metroPanel1.Theme = MetroThemeStyle.Light;
+            metroRadioButton1.Theme = MetroThemeStyle.Light;
+            metroRadioButton2.Theme = MetroThemeStyle.Light;
+        }
+
+        private void MetroRadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Theme = MetroThemeStyle.Dark;
+            metroButton1.Theme = MetroThemeStyle.Dark;
+            metroButton2.Theme = MetroThemeStyle.Dark;
+            metroButton3.Theme = MetroThemeStyle.Dark;
+            metroButton4.Theme = MetroThemeStyle.Dark;
+            metroTextBox1.Theme = MetroThemeStyle.Dark;
+            metroTextBox2.Theme = MetroThemeStyle.Dark;
+            metroPanel1.Theme = MetroThemeStyle.Dark;
+            metroRadioButton1.Theme = MetroThemeStyle.Dark;
+            metroRadioButton2.Theme = MetroThemeStyle.Dark;
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
